fff69f9b8b0330f031ca2f5b046c05601c2e4862
Ayomide Philip <a.areo.csc2023064@student.oauife.edu.ng>
2025-12-30 00:42:57 +0100

    feat: implement poll joining functionality with database connection

--- app/api/polls/[pollsId]/join/route.js
+++ app/api/polls/[pollsId]/join/route.js
@@ -1,8 +1,19 @@
 import { NextResponse } from "next/server";
+import { connectDatabase } from "@/libs/connectdatabase";
 
 export async function PUT(req, { params }) {
   const { userId } = await req.json();
+  const { pollsId } = await params;
   try {
+    await connectDatabase();
+    // get the poll
+
+    return NextResponse.json(
+      { message: "Successfully updated poll", userId, pollsId },
+      {
+        status: 200,
+      }
+    );
   } catch (err) {
     console.log(err);
     return NextResponse.json(
